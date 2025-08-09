343b890347824bc94ba313c6d649d69f3a40c04d
lndsilva <lndsilva@hotmail.com>
2025-08-08 22:12:28 -0300

    criando view

--- app/src/main/java/br/com/etecia/myapp/AdaptadorProdutos.java
+++ app/src/main/java/br/com/etecia/myapp/AdaptadorProdutos.java
@@ -1,16 +1,33 @@
 package br.com.etecia.myapp;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class AdaptadorProdutos extends RecyclerView.Adapter<AdaptadorProdutos.ViewHolder> {
+    private Context context;
+    private List<Produtos> lstProdutos;
+
+    public AdaptadorProdutos(Context context, List<Produtos> lstProdutos) {
+        this.context = context;
+        this.lstProdutos = lstProdutos;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.modelo_produtos, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
@@ -20,13 +37,19 @@ public class AdaptadorProdutos extends RecyclerView.Adapter<AdaptadorProdutos.Vi
 
     @Override
     public int getItemCount() {
-        return 0;
+        return lstProdutos.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        CardView cardProdutos;
+        ImageView modImagemProduto;
+        TextView modTituloProduto;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            cardProdutos = itemView.findViewById(R.id.cardProdutos);
+            modImagemProduto = itemView.findViewById(R.id.modImagemProduto);
+            modTituloProduto = itemView.findViewById(R.id.modTituloProduto);
         }
     }
 }
